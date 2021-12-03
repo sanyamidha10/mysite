43ce2294f10a29ce5270b59126c52496445963cc
sanya midha <sanyamidha.10@gmail.com>
2021-12-03 14:38:14 +0530

    changed some font styling

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/login', LoginView.as_view(template_name='registeration/login.html'), name='login'),
-    path('accounts/logout', LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
+    path('accounts/login/', LoginView.as_view(template_name='registeration/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
 ]
